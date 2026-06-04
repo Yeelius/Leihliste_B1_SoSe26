250a899ba671d75639085c65d6c7a77205c50e10
Yeelius <julius.neubert777@gmail.com>
2026-06-04 17:44:35 +0200

    Delete src/Walking_Skeleton/Persistenz directory
    Zusammengefügt in neuem Ordner: Persistenz-Backend-Frontend

--- src/Walking_Skeleton/Persistenz/local_persistence_skeleton/backend/persistence/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Note
-
-class NoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Note
-        fields = ["id", "text", "created_at"]
-        read_only_fields = ["id", "created_at"]
--- src/Walking_Skeleton/Persistenz/local_persistence_skeleton/backend/persistence/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import NoteListCreateView
-
-urlpatterns = [
-    path("notes/", NoteListCreateView.as_view(), name="notes-list-create"),
-]
